e7bd91214639021a8c42efb54879288c12ffa9a4
Surik4t <surik4t@yandex.ru>
2025-07-12 12:31:32 +0700

    add remove order api

--- backend/app/api.py
+++ backend/app/api.py
@@ -3,6 +3,7 @@ from configurations import collection, client
 from database.schemas import individual_order
 from database.models import Order
 from fastapi.middleware.cors import CORSMiddleware
+from bson.objectid import ObjectId
 
 
 app = FastAPI()
@@ -39,7 +40,7 @@ async def health_check():
     return {"message": "Server is working fine."}
 
 
-@router.get("/")
+@router.get("/", tags=["orders"])
 async def get_all_orders():
     result = list()
     async for order in collection.find():
@@ -47,12 +48,25 @@ async def get_all_orders():
     return result
 
 
-@router.post("/")
+@router.post("/", tags=["orders"])
 async def create_order(new_order: Order):
     try:
         cursor = await collection.insert_one(dict(new_order))
         return {"status_code": 200, "id": str(cursor)}
     except Exception as e:
         return HTTPException(status_code=500, detail=f"Could not create an order: {e}")
+
+
+@router.delete("/{order_id}", tags=["orders"])
+async def remove_order(order_id):
+    try:
+        order = await collection.find_one({"_id": ObjectId(order_id)})
+        if order:
+            await collection.delete_one({"_id": ObjectId(order_id)})
+            return {"message": "Order removed."}
+        else:
+            return HTTPException(status_code=404, detail="Order not found.")
+    except Exception as e:
+        return HTTPException(status_code=500, detail=f"An error occured: {e}")
 
 app.include_router(router)
