72928109fd88387fc69a34d89746a2fabb19f5bc
Surik4t <surik4t@yandex.ru>
2025-07-14 18:38:13 +0700

    delete unused dependencies

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,6 @@
-import { ChakraProvider, createSystem, defineConfig, defaultConfig } from "@chakra-ui/react";
-import { defaultSystem } from "@chakra-ui/react"
+import { ChakraProvider } from "@chakra-ui/react";
+import { defaultSystem } from "@chakra-ui/react";
 import Background from "./components/Background";
-import Order from './components/Order';
-import Demo from "./components/Creator";
 
 function App() {
 
