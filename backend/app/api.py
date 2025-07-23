b2a64125418c56821690cb6b82d4b2dd750b45fd
Surik4t <surik4t@yandex.ru>
2025-07-23 14:17:02 +0700

    refactor: move order routes to services/Orders.py

--- backend/app/api.py
+++ backend/app/api.py
@@ -1,20 +1,14 @@
 from fastapi import FastAPI, APIRouter, HTTPException
-from configurations import orders_collection, client
-from .services import Ingredients
-from database.schemas import order_schema
-from database.models import OrderModel
+from configurations import client
+from .services import Ingredients, Orders
 from fastapi.middleware.cors import CORSMiddleware
-from bson.objectid import ObjectId
 
 
 app = FastAPI()
 router = APIRouter()
 
 
-origins = [
-    "http://localhost:5173",
-    "localhost:5173"
-]
+origins = ["http://localhost:5173", "localhost:5173"]
 
 
 app.add_middleware(
@@ -22,7 +16,7 @@ app.add_middleware(
     allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=["*"]
+    allow_headers=["*"],
 )
 
 
@@ -41,35 +35,6 @@ async def health_check():
     return {"message": "Server is working fine."}
 
 
-@router.get("/", tags=["orders"])
-async def get_all_orders():
-    result = list()
-    async for order in orders_collection.find():
-        result.append(order_schema(order))
-    return result
-
-
-@router.post("/", tags=["orders"])
-async def create_order(new_order: OrderModel):
-    try:
-        cursor = await orders_collection.insert_one(dict(**new_order.model_dump()))
-        return {"status_code": 200, "message": f"Order id: {cursor.inserted_id} created."}
-    except Exception as e:
-        return HTTPException(status_code=500, detail=f"Could not create an order: {e}")
-
-
-@router.delete("/{order_id}", tags=["orders"])
-async def remove_order(order_id):
-    try:
-        order = await orders_collection.find_one({"_id": ObjectId(order_id)})
-        if order:
-            await orders_collection.delete_one({"_id": ObjectId(order_id)})
-            return {"message": "Order removed."}
-        else:
-            return HTTPException(status_code=404, detail="Order not found.")
-    except Exception as e:
-        return HTTPException(status_code=500, detail=f"An error occured: {e}")
-
-
 app.include_router(router)
+app.include_router(Orders.router)
 app.include_router(Ingredients.router)
---
+++ backend/app/services/Orders.py
@@ -0,0 +1,40 @@
+from fastapi import APIRouter, HTTPException
+from configurations import orders_collection
+from database.schemas import order_schema
+from database.models import OrderModel
+from bson.objectid import ObjectId
+
+router = APIRouter(prefix="/orders", tags=["orders"])
+
+
+@router.get("")
+async def get_all_orders():
+    result = list()
+    async for order in orders_collection.find():
+        result.append(order_schema(order))
+    return result
+
+
+@router.post("")
+async def create_order(new_order: OrderModel):
+    try:
+        cursor = await orders_collection.insert_one(dict(**new_order.model_dump()))
+        return {
+            "status_code": 200,
+            "message": f"Order id: {cursor.inserted_id} created.",
+        }
+    except Exception as e:
+        return HTTPException(status_code=500, detail=f"Could not create an order: {e}")
+
+
+@router.delete("/{order_id}")
+async def remove_order(order_id):
+    try:
+        order = await orders_collection.find_one({"_id": ObjectId(order_id)})
+        if order:
+            await orders_collection.delete_one({"_id": ObjectId(order_id)})
+            return {"message": "Order removed."}
+        else:
+            return HTTPException(status_code=404, detail="Order not found.")
+    except Exception as e:
+        return HTTPException(status_code=500, detail=f"An error occured: {e}")
