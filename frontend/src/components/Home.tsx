e42900f191958017207ef8424c0e8a9339e0415a
Surik4t <surik4t@yandex.ru>
2025-12-01 06:35:49 +0700

    feat: save current tab value to local storage

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -1,22 +1,9 @@
 import { Flex } from "@chakra-ui/react"
 import Header from "./Header";
 import { Menu as MainMenu } from './Menu';
-import { useAuth } from "../contexts/AuthContext"
-import { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+
 
 function Home() {
-    // const { isAuthenticated, isLoading } = useAuth();
-    // const navigate = useNavigate();
-
-    // useEffect(() => {
-    //         if (!isLoading && !isAuthenticated) {
-    //             navigate('/login');
-    //         }
-    //     }, [isAuthenticated, isLoading, navigate]);
-
-    // if (isLoading) return <div>Loading...</div>;
-    // if (!isAuthenticated) return null;
     
     return (
         <Flex bg="#f8ebd7ff" colorPalette="orange" minHeight="100vh">
