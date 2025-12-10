a07a36051931d300e95da8b926c14230e80cedf4
Surik4t <surik4t@yandex.ru>
2025-12-10 16:41:05 +0700

    update db_init

--- backend/database/db_init.py
+++ backend/database/db_init.py
@@ -1,60 +1,67 @@
 import pymongo
 
-ingredients = [{
-  "name": "Bun",
-  "price": 0.39,
-  "weight": 50
-},
-{
-  "name": "Beef Patty",
-  "price": 2.49,
-  "weight": 120
-},
-{
-  "name": "Chicken Nuggets",
-  "price": 1.99,
-  "weight": 100
-},
-{
-  "name": "Tomato Slice",
-  "price": 0.49,
-  "weight": 30
-},
-{
-  "name": "Mayo",
-  "price": 0.09,
-  "weight": 15
-},
-{
-  "name": "Ketchup",
-  "price": 0.09,
-  "weight": 15
-},
-{
-  "name": "Mustard",
-  "price": 0.09,
-  "weight": 10
-},
-{
-  "name": "Cheese",
-  "price": 0.39,
-  "weight": 25
-},
-{
-  "name": "Pickles",
-  "price": 0.29,
-  "weight": 15
-},
-{
-  "name": "Lettuce",
-  "price": 0.39,
-  "weight": 20
-},
-{
-  "name": "Top Bun",
-  "price": 0.39,
-  "weight": 50
-}]
+ingredients = [
+    {
+        "name": "Bottom Bun",
+        "price": 0.39,
+        "weight": 50
+    },
+    {
+        "name": "Top Bun",
+        "price": 0.39,
+        "weight": 50
+    },
+    {
+        "name": "Beef Patty",
+        "price": 2.49,
+        "weight": 120
+    },
+    {
+        "name": "Chicken",
+        "price": 2.29,
+        "weight": 100
+    },
+    {
+        "name": "Tomato Slice",
+        "price": 0.69,
+        "weight": 30
+    },
+    {
+        "name": "Mayo",
+        "price": 0.09,
+        "weight": 15
+    },
+    {
+        "name": "Ketchup",
+        "price": 0.09,
+        "weight": 15
+    },
+    {
+        "name": "Mustard",
+        "price": 0.09,
+        "weight": 10
+    },
+    {
+        "name": "Cheese",
+        "price": 0.79,
+        "weight": 25
+    },
+    {
+        "name": "Onions",
+        "price": 0.39,
+        "weight": 15
+    },
+    {
+        "name": "Pickles",
+        "price": 0.29,
+        "weight": 15
+    },
+    {
+        "name": "Lettuce",
+        "price": 0.49,
+        "weight": 20
+    },
+]
 
 host = "mongodb://localhost:27017"
 
@@ -64,6 +71,7 @@ db = client["CreateYourOwnBurgerDB"]
 collection = db["IngredientsCollection"]
 
 try:
+    collection.delete_many({})
     collection.insert_many(ingredients)
     print("db initialization successful.")
 except Exception as e:
