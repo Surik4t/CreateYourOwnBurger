526dd1e5d4aba6a13e222724cc522aceb801581c
Surik4t <surik4t@yandex.ru>
2025-12-10 18:34:39 +0700

    update 'About' tab: add link to source code and contact info

--- frontend/src/components/About.tsx
+++ frontend/src/components/About.tsx
@@ -9,12 +9,8 @@ const About = () => {
                     About the <b>"Create Your Own Burger"</b> app.
                 </Text>
                 <Text mt="1em" textStyle="2xl" textAlign="center">
-                    Create Your Own Burger is a web application for designing, customizing, and ordering fully personalized burgers, giving you ultimate control over your meal.
+                    "Create Your Own Burger" is a web application for designing, customizing, and ordering fully personalized burgers, giving you ultimate control over your meal.
                 </Text>
-                <Text textStyle="2xl" textAlign="center">
-                    This is a web app for burger enthusiasts and picky eaters who want to craft their perfect meal by choosing every ingredient, then order it for delivery or pickup.
-                </Text>
-
     
                 <Text mt="2em" mb="1em" textStyle="3xl" textAlign="center">
                     <b>Tech stack:</b>
@@ -79,6 +75,19 @@ const About = () => {
                     </Table.Root>
                 </Flex>
 
+                <Text mt="2em" textStyle="2xl" textAlign="center">
+                    Check out the <Link href="https://github.com/Surik4t/CreateYourOwnBurger">Source Code!</Link>
+                </Text>
+
+                <Text mt="2em" textStyle="2xl" textAlign="center">
+                    Thank you for using the app!
+                </Text>
+                <Text textStyle="2xl" textAlign="center">
+                    Got any questions or feedback?
+                </Text>
+                <Text textStyle="2xl" textAlign="center">
+                    Feel free to contact me at <Link href="mailto:Surik4t@yandex.ru?subject=CreateYourOwnBurgerApp">Surik4t@yandex.ru</Link>
+                </Text>
             </Flex>
         </Flex>
     )
